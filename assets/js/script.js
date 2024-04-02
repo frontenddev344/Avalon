59aff597a8e01db2d13a6a75d0f9fab650511afc
frontenddev344 <devavology12@gmail.com>
2024-04-02 17:18:10 +0530

    change

--- assets/js/script.js
+++ assets/js/script.js
@@ -14,3 +14,25 @@ function openCity(evt, cityName) {
   
   // Get the element with id="defaultOpen" and click on it
   document.getElementById("defaultOpen").click();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
